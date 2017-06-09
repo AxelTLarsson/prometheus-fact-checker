4d45ccab7f0f0bf120dd314afc1ba0474fce7104
AxelTLarsson <mail@axellarsson.nu>
2017-06-09 13:40:06 +0200

    Refactor code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class FactChecker(object):
     label_cache = {}
 
     def label_for(self, q):
-        print(f"labeling {q}")
+        cherrypy.log(f"labeling {q}")
         q = q.upper()
         if q in self.label_cache:
             return self.label_cache[q]
@@ -34,62 +34,64 @@ class FactChecker(object):
     @cherrypy.tools.json_out()
     def check(self, url=None):
         if url is not None:
-            print(f"GET {url}")
+            # GET the page
+            cherrypy.log(f"GET {url}")
             try:
                 page = requests.get(url)
             except requests.exceptions.RequestException as e:
-                print(f"Could not get url: {url}")
+                cherrypy.log(f"Could not get url: {url}")
                 cherrypy.response.status = '503'
                 return f"Could not get the requested url: {url}"
 
-            print("Extracting relations from Prometheus...")
-
+            # Connect to Prometheus
+            cherrypy.log("Extracting relations from Prometheus...")
             try:
                 relations = requests.post('http://localhost:8080/api/en/extract', data=page).json()
                 #  relations = json.loads('[{ "subject": "Q76", "predictedPredicate": "P26", "obj": "Q13133", "sentence": "bla bla", "source": "eh", "probability": "0.99" }]')
-                print("relations extracted: %s" % relations)
-
+                cherrypy.log("relations extracted: %s" % relations)
                 # group extracted relations together
-                def keyfunc(relation):
-                    return (relation['subject'], relation['predictedPredicate'], relation['obj'])
-                extractions = []
-                data = sorted(relations, key=keyfunc)
-                for k, g in groupby(data, keyfunc):
-                    extractions.append(list(g))      # Store group iterator as a list
-                results = []
-                for extraction in extractions:
-                    # only check once per actual relation
-                    print(extraction[0])
-                    evidence = self.check_relation(extraction[0])
-                    result = {}
-                    result['subject'] = {
-                        'name': self.label_for(extraction[0]['subject']),
-                        'link': self.link_for(extraction[0]['subject'])
-                    }
-                    result['object'] = {
-                        'name': self.label_for(extraction[0]['obj']),
-                        'link': self.link_for(extraction[0]['obj'])
-                    }
-                    result['predicate'] = {
-                        'name': self.label_for(extraction[0]['predictedPredicate']),
-                        'link': self.link_for(extraction[0]['predictedPredicate'])
-                    }
-                    result['sentences'] = list(map(lambda r: r['sentence'], extraction))
-                    result['type'] = evidence[0]
-                    print("tampering with evidence")
-                    for match in evidence[1]:
-                        match['subject'] = self.label_for(match['subject'])
-                        match['predictedPredicate'] = self.label_for(match['predictedPredicate'])
-                        match['obj'] = self.label_for(match['obj'])
-                    result['evidence'] = list(map(lambda e: self.trim_evidence(e), evidence[1]))
-                    results.append(result)
 
-                return results
-
-            except ConnectionError as e:
-                print("An error ocurred while connecting to Prometheus")
+            except:
+                cherrypy.log("An error ocurred while connecting to Prometheus")
                 cherrypy.response.status = '503'
                 return "An error occurred while connecting to Prometheus"
+
+            # Group extracted relations by relation triple
+            def keyfunc(relation):
+                return (relation['subject'], relation['predictedPredicate'], relation['obj'])
+
+            extractions = []
+            data = sorted(relations, key=keyfunc)
+            for k, g in groupby(data, keyfunc):
+                extractions.append(list(g))      # Store group iterator as a list
+            results = []
+            for extraction in extractions:
+                # only check once per actual relation
+                evidence = self.check_relation(extraction[0])
+                result = {}
+                result['subject'] = {
+                    'name': self.label_for(extraction[0]['subject']),
+                    'link': self.link_for(extraction[0]['subject'])
+                }
+                result['object'] = {
+                    'name': self.label_for(extraction[0]['obj']),
+                    'link': self.link_for(extraction[0]['obj'])
+                }
+                result['predicate'] = {
+                    'name': self.label_for(extraction[0]['predictedPredicate']),
+                    'link': self.link_for(extraction[0]['predictedPredicate'])
+                }
+                result['sentences'] = list(map(lambda r: r['sentence'], extraction))
+                result['type'] = evidence[0]
+                cherrypy.log("tampering with evidence")
+                for match in evidence[1]:
+                    match['subject'] = self.label_for(match['subject'])
+                    match['predictedPredicate'] = self.label_for(match['predictedPredicate'])
+                    match['obj'] = self.label_for(match['obj'])
+                result['evidence'] = list(map(lambda e: self.trim_evidence(e), evidence[1]))
+                results.append(result)
+
+            return results
 
         else:
             cherrypy.response.status = '400'
